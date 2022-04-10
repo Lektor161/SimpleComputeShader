20baeaf5ed65718e7642c058d07792639ae7436f
Aleksey <a.kholodov@niuitmo.ru>
2022-04-11 01:11:30 +0300

    Variables hot swap. Camera Render. Blur xy

--- Assets/Radar/RadarScript.cs
+++ Assets/Radar/RadarScript.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEditor;
 using UnityEngine;
 
 namespace Radar
@@ -12,10 +17,22 @@ namespace Radar
         public Camera depthCam;
 
         [SerializeField]
+        public float cameraFar = 300f;
+        [SerializeField]
+        public float cameraNear = 0.3f;
+        [SerializeField]
         public float cameraHorizontalAngle = 20;
         [SerializeField]
         public float cameraVerticalAngle = 60;
+
+        [SerializeField]
+        public int blurAngle;
+        [SerializeField]
+        public int blurRadius;
+        [SerializeField]
+        public float cameraRotationSpeed;
         
+        [SerializeField]
         public GameObject radar;
         [SerializeField]
         public ComputeShader shader;
@@ -33,11 +50,9 @@ namespace Radar
         private int _kernelID;
         private int _camWidth, _camHeight;
         private float _verAngle, _horAngle;
-        private float _camNear, _camFar;
 
         public int textureWidth = 1024;
         public int textureHeight = 1024;
-        public int blurRadius = 1;
         
         private int _fragmentNum;
         private float _curAngle = 0;
@@ -53,29 +68,9 @@ namespace Radar
             _generateTextureKernelID = shader.FindKernel("generate_texture");
             _clearBufferKernelID = shader.FindKernel("clear_buffer");
             _blurKernelID = shader.FindKernel("blur");
-        
-            print(depthCam.fieldOfView);
-            _camNear = depthCam.nearClipPlane;
-            _camFar = depthCam.farClipPlane;
+            
             _camWidth = depthCam.pixelWidth;
             _camHeight = depthCam.pixelHeight;
-
-            _verAngle = cameraVerticalAngle * Mathf.Deg2Rad;
-            _horAngle = cameraHorizontalAngle * Mathf.Deg2Rad;
-            depthCam.fieldOfView = cameraVerticalAngle;
-            depthCam.aspect = Mathf.Tan(_horAngle / 2) / Mathf.Tan(_verAngle / 2);
-            colorCam.fieldOfView = cameraVerticalAngle;
-            colorCam.aspect = Mathf.Tan(_horAngle / 2) / Mathf.Tan(_verAngle / 2);
-            
-            
-            //_verAngle = depthCam.fieldOfView * Mathf.Deg2Rad;
-            //_horAngle = 2 * Mathf.Atan(Mathf.Tan(_verAngle * 0.5f) * depthCam.aspect);
-
-            _fragmentNum = textureHeight;
-
-            _fragmentLength = _camFar / _fragmentNum *
-                Mathf.Sqrt(1 + Mathf.Pow(Mathf.Tan(_horAngle / 2), 2) + 
-                           Mathf.Pow(Mathf.Tan(_verAngle / 2), 2));
 
             _colorTexture = CreateTexture(_camWidth, _camHeight, 0, RenderTextureFormat.ARGBFloat, false);
             _depthTexture = CreateTexture(_camWidth, _camHeight, 24, RenderTextureFormat.Depth, false);
@@ -84,8 +79,6 @@ namespace Radar
             
             colorCam.targetTexture = _colorTexture;
             depthCam.targetTexture = _depthTexture;
-            //cam.SetTargetBuffers(_colorTexture.colorBuffer, _depthTexture.depthBuffer);
-
             _buffer = new ComputeBuffer(_camWidth * _fragmentNum, sizeof(int));
         
             shader.SetTexture(_generateBufferKernelID, "color_texture", _colorTexture);
@@ -94,41 +87,36 @@ namespace Radar
             shader.SetBuffer(_generateBufferKernelID, "buffer", _buffer);
             shader.SetBuffer(_generateTextureKernelID, "buffer", _buffer);
             shader.SetBuffer(_clearBufferKernelID, "buffer", _buffer);
-        
-            shader.SetFloat("near", _camNear);
-            shader.SetFloat("far", _camFar);
-            shader.SetFloats("cam_angle", _horAngle, _verAngle);
+            
             shader.SetInt("cam_width", _camWidth);
             shader.SetInt("cam_height", _camHeight);
-            shader.SetFloat("fragment_length", _fragmentLength);
-            shader.SetInt("fragment_num", _fragmentNum);
-            shader.SetFloat("max_radius", 0.5f * textureWidth);
             shader.SetInt("texture_width", textureWidth);
             shader.SetInt("texture_height", textureHeight);
-            shader.SetFloat("color_norm_const", colorNormConst);
             shader.SetFloat("cur_angle", _curAngle);
             
             shader.SetTexture(_blurKernelID, "result_texture", _radarTexture);
             shader.SetTexture(_blurKernelID, "blur_texture", _blurTexture);
-            shader.SetInt("blur_radius", blurRadius);
+            colorCam.enabled = false;
+            depthCam.enabled = false;
         }
 
         void FixedUpdate()
         {
+            colorCam.Render();
+            depthCam.Render();
             var angles = _horAngle / 2 / Mathf.PI * textureWidth;
             Dispatch(shader, _generateBufferKernelID, _camWidth, _camHeight);
             Dispatch(shader, _generateTextureKernelID, (int) angles, textureHeight);
             Dispatch(shader, _blurKernelID, (int) angles, textureHeight);
             Dispatch(shader, _clearBufferKernelID, _camWidth, textureHeight); 
             
-            cams.transform.Rotate(0, _horAngle / 2 * Mathf.Rad2Deg, 0);
-            _curAngle += _horAngle / 2;
+            _curAngle += cameraRotationSpeed * Mathf.Deg2Rad * Time.deltaTime;
             shader.SetFloat("cur_angle", _curAngle % (2 * Mathf.PI));
+            cams.transform.eulerAngles = new Vector3(0, _curAngle * Mathf.Rad2Deg, 0);
             
-            //radar.GetComponent<MeshRenderer>().material.SetTexture("_Texture", _radarTexture);
             radar.GetComponent<MeshRenderer>().material.SetTexture("_Texture", _blurTexture);
         }
-        private void Dispatch(ComputeShader shader, int kernelID, int width, int height)
+        private static void Dispatch(ComputeShader shader, int kernelID, int width, int height)
         {
             shader.Dispatch(kernelID, (width + 7) / 8, (height + 7) / 8, 1);
         }
@@ -143,6 +131,36 @@ namespace Radar
             return texture;
         }
 
+        
+        public void OnValidate()
+        {
+            depthCam.nearClipPlane = cameraNear;
+            depthCam.farClipPlane = cameraFar;
+            colorCam.nearClipPlane = cameraNear;
+            colorCam.farClipPlane = cameraFar;
+            
+            _verAngle = cameraVerticalAngle * Mathf.Deg2Rad;
+            _horAngle = cameraHorizontalAngle * Mathf.Deg2Rad;
+            depthCam.fieldOfView = cameraVerticalAngle;
+            depthCam.aspect = Mathf.Tan(_horAngle / 2) / Mathf.Tan(_verAngle / 2);
+            colorCam.fieldOfView = cameraVerticalAngle;
+            colorCam.aspect = Mathf.Tan(_horAngle / 2) / Mathf.Tan(_verAngle / 2);
+            
+            shader.SetFloat("color_norm_const", colorNormConst);
+            shader.SetFloats("cam_angle", _horAngle, _verAngle);
+            shader.SetInts("blurxy", blurAngle, blurRadius);
+            
+            _fragmentNum = textureHeight;
+            _fragmentLength = cameraFar / _fragmentNum *
+                              Mathf.Sqrt(1 + Mathf.Pow(Mathf.Tan(_horAngle / 2), 2) + 
+                                         Mathf.Pow(Mathf.Tan(_verAngle / 2), 2));
+            shader.SetInt("fragment_num", _fragmentNum);
+            shader.SetFloat("fragment_length", _fragmentLength);
+            
+            shader.SetFloat("near", cameraNear);
+            shader.SetFloat("far", cameraFar);
+        }
+        
         private void OnDestroy()
         {
             _buffer.Dispose();
