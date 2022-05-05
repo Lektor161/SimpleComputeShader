f11509680bf0e15e9c72e70f67e95a100214eb42
Aleksey <a.kholodov@niuitmo.ru>
2022-05-05 14:00:51 +0300

    minor upd

--- Assets/Radar/Scripts/SetUnlitMaterial.cs
+++ Assets/Radar/Scripts/SetUnlitMaterial.cs
@@ -15,16 +15,16 @@ public class SetUnlitMaterial : MonoBehaviour
         var random = new Random();
         var ts = mainObject.GetComponentsInChildren<Transform>();
         if (ts == null) return;
-        var blueVal = 50;
+        //var blueVal = 50;
         foreach (var t in ts)
         {
-            blueVal = (blueVal + 1) % 256;
-            blueVal = Math.Max(blueVal, 50);
+            //blueVal = (blueVal + 1) % 256;
+            //blueVal = Math.Max(blueVal, 50);
             if (t == null || t.gameObject == null) continue;
             if (!t.gameObject.TryGetComponent(out Renderer render)) continue;
             var material = new Material(shader);
             //material.SetColor("_Color", Color.red + Color.green / 255 * blueVal);
-            material.SetColor("_Color", Color.red + Color.green / 255 * (100 + random.Next(150)));
+            material.SetColor("_Color", Color.red + Color.green / 255 * (20 + random.Next(230)));
             render.material = material;
         }
     }
